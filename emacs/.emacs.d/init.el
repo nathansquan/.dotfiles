346ccc3bacf2c328e54acb3d72c3bcb0c837b1d2
Nathan Quan <nathansquan@gmail.com>
2022-12-30 12:27:51 -0500

    stow emacs config file

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,10 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; Themes
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+;; (load-theme 'nord t)
