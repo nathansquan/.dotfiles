531db3ae099dd885c117f0c6c1a120c0c357fb1d
Nathan Quan <nathansquan@gmail.com>
2025-04-15 18:22:17 -0400

    add telescope keymaps

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -17,21 +17,30 @@ return {
       require("telescope").load_extension("fzf")
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-      vim.keymap.set("n", "<leader>en", function()
-        builtin.find_files({
-          cwd = vim.fn.stdpath("config"),
+      vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Telescope [S]earch [H]elp" })
+      vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Telescope [S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Telescope [S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+      vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Telescope [S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Telescope [S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Telescope [S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Telescope [S]earch [R]esume" })
+      vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = 'Telescope [S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope [ ] Find existing buffers" })
+      vim.keymap.set("n", "<leader>s/", function()
+        builtin.live_grep({
+          grep_open_files = true,
+          prompt_title = "Live Grep in Open Files",
         })
-      end, { desc = "Telescope find nvim config files" })
-      -- Edit packages
+      end, { desc = "Telescope [S]earch [/] in Open Files" })
+      vim.keymap.set("n", "<leader>sn", function()
+        builtin.find_files({ cwd = vim.fn.stdpath("config") })
+      end, { desc = "Telescope [S]earch [N]eovim Files" })
       vim.keymap.set("n", "<leader>ep", function()
         builtin.find_files({
           cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
         })
-      end, { desc = "Telescope Lazy package sources" })
+      end, { desc = "Telescope [S]earch Lazy [P]ackage Sources" })
 
       require("config.telescope.multigrep").setup()
     end,
