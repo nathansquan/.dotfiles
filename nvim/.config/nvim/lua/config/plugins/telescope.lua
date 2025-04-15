0e123792a8977bc650ec6d6a5cab454086437d21
Nathan Quan <nathansquan@gmail.com>
2025-04-14 23:13:18 -0400

    update telescope

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,35 +1,39 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+  { -- Fuzzy Finder (files, lsp, etc)
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    event = "VimEnter",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
-      require('telescope').setup {
+      require("telescope").setup({
         extensions = {
-          fzf = {}
-        }
-      }
+          fzf = {},
+        },
+      })
 
-      require('telescope').load_extension('fzf')
+      require("telescope").load_extension("fzf")
 
-      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
-      vim.keymap.set("n", "<space>ff", require('telescope.builtin').find_files)
-      vim.keymap.set("n", "<space>en", function()
-        require('telescope.builtin').find_files {
-          cwd = vim.fn.stdpath("config")
-        }
-      end)
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files({
+          cwd = vim.fn.stdpath("config"),
+        })
+      end, { desc = "Telescope find nvim config files" })
       -- Edit packages
-      vim.keymap.set("n", "<space>ep", function()
-        require('telescope.builtin').find_files {
-          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
-        }
-      end)
+      vim.keymap.set("n", "<leader>ep", function()
+        builtin.find_files({
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+        })
+      end, { desc = "Telescope Lazy package sources" })
 
       require("config.telescope.multigrep").setup()
-    end
-  }
+    end,
+  },
 }
